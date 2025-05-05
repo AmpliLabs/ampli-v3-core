3b2e658a756e5f9852c232bdf02ae9213490a91c
wangshouh <wongshouhao@outlook.com>
2025-05-06 00:32:35 +0800

    Fix safe cast

--- src/libraries/SafeCast.sol
+++ src/libraries/SafeCast.sol
@@ -7,7 +7,7 @@ library SafeCast {
     /// @notice Cast a int256 to a int128, revert on overflow or underflow
     /// @param x The int256 to be downcasted
     /// @return y The downcasted integer, now type int128
-    function toInt128(uint256 x) internal pure returns (uint128 y) {
+    function toInt128(uint256 x) internal pure returns (int128 y) {
         require(x < 1 << 127, SafeCastOverflow());
         y = int128(int256(x));
     }
