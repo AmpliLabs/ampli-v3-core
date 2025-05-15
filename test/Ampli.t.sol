76df1b0d28b8a648d62a3d9aaad5c21f882000c5
wangshouh <wongshouhao@outlook.com>
2025-05-16 01:35:26 +0800

    Add basic swap and add liquidity

--- test/Ampli.t.sol
+++ test/Ampli.t.sol
@@ -2,13 +2,16 @@
 pragma solidity ^0.8.0;
 
 import {Test, console} from "forge-std/Test.sol";
+import {Deployers} from "./utils/Deployers.sol";
 import {Actions} from "./utils/ActionsRouter.sol";
+import {V4Actions} from "./utils/V4MiniRouter.sol";
 import {Pool} from "src/types/Pool.sol";
 import {BorrowShare, BorrowShareLibrary} from "src/types/BorrowShare.sol";
-import {Deployers} from "./utils/Deployers.sol";
 import {Currency} from "v4-core/types/Currency.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {TickMath} from "v4-core/libraries/TickMath.sol";
 import {IHooks} from "v4-core/interfaces/IHooks.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {IERC20} from "src/interfaces/IERC20.sol";
 
 contract PoolTest is Test, Deployers {
@@ -18,26 +21,33 @@ contract PoolTest is Test, Deployers {
 
     function setUp() public {
         vm.createSelectFork("Base", 30177975);
-        deployAmpliWithActionRouter();
         deployFreshManager();
+        deployV4MiniRouter();
+        deployAmpliWithActionRouter();
         deployMockERC20();
         deployIrmAndOracle();
 
         poolKey = PoolKey({
-            currency0: Currency.wrap(address(0x03e213441dCcC2344E11D7b713FeaAAe4E60813D)),
+            currency0: Currency.wrap(address(0xA19570ac9E7bB35eAABe7a2e97B2941c200b3667)),
             currency1: Currency.wrap(address(tokenMock)),
             fee: 100, // 0.01%
             tickSpacing: 1,
             hooks: IHooks(address(0xFb46d30c9B3ACc61d714D167179748FD01E09aC0))
         });
 
-        pegToken = IERC20(address(0x03e213441dCcC2344E11D7b713FeaAAe4E60813D));
+        pegToken = IERC20(address(0xA19570ac9E7bB35eAABe7a2e97B2941c200b3667));
         actionsRouter.approve(address(tokenMock));
         ampli.initialize(address(tokenMock), address(this), irm, oracle, 2, 1, hex"ff");
 
         irm.setBorrowRate(0.01 * 1e27);
         // assetId = 0, mock token price / peg token = 1
         oracle.setFungibleAssetPrice(0, 1e36);
+
+        // Approve
+        tokenMock.approve(address(ampli), type(uint256).max);
+        tokenMock.approve(address(actionsRouter), type(uint256).max);
+        tokenMock.approve(address(v4MiniRouter), type(uint256).max);
+        pegToken.approve(address(v4MiniRouter), type(uint256).max);
     }
 
     function test_supplyAndWithdrawFungibleCollateral() public {
@@ -59,7 +69,6 @@ contract PoolTest is Test, Deployers {
         params[3] = abi.encode(address(tokenMock), address(this), 1 ether);
 
         tokenMock.mint(address(this), 1 ether);
-        tokenMock.approve(address(actionsRouter), 1 ether);
 
         actionsRouter.executeActions(actions, params);
 
@@ -69,8 +78,6 @@ contract PoolTest is Test, Deployers {
     function test_borrow() public {
         ampli.updateAuthorization(poolKey, 1, address(this), address(actionsRouter));
         tokenMock.mint(address(this), 10 ether);
-        tokenMock.approve(address(ampli), 10 ether);
-        tokenMock.approve(address(actionsRouter), 10 ether);
 
         ampli.supplyFungibleCollateral(poolKey, 1, 0, 10 ether);
         BorrowShare borrowed = BorrowShareLibrary.toSharesDown(1 ether, 0, BorrowShare.wrap(0));
@@ -84,5 +91,35 @@ contract PoolTest is Test, Deployers {
         actionsRouter.executeActions(actions, params);
 
         assertEq(pegToken.balanceOf(address(actionsRouter)), 1 ether);
+    }
+
+    function _supplyMaxFungibleCollateral(uint256 amount) public {
+        ampli.updateAuthorization(poolKey, 1, address(this), address(actionsRouter));
+
+        ampli.supplyFungibleCollateral(poolKey, 1, 0, amount);
+    }
+
+    function _borrowPegToken(uint256 amount) public {
+        BorrowShare borrowed = BorrowShareLibrary.toSharesDown(amount, 0, BorrowShare.wrap(0));
+
+        Actions[] memory actions = new Actions[](2);
+        bytes[] memory params = new bytes[](2);
+
+        actions[0] = Actions.BORROW;
+        params[0] = abi.encode(poolKey, 1, borrowed);
+
+        actions[1] = Actions.TRANSFER_OUT_FUNGIBLE_ASSET;
+        params[1] = abi.encode(address(pegToken), address(this), amount);
+
+        actionsRouter.executeActions(actions, params);
+    }
+
+    function test_swap() public {
+        tokenMock.mint(address(this), 1500 ether);
+        _supplyMaxFungibleCollateral(1000 ether);
+        _borrowPegToken(600 ether);
+        v4RouterHelper.addLiquidity(address(this), poolKey);
+
+        v4RouterHelper.swap(address(this), poolKey, 10 ether);
     }
 }
--- test/utils/Deployers.sol
+++ test/utils/Deployers.sol
@@ -8,6 +8,8 @@ import {TestERC20} from "test/mock/TestERC20.sol";
 import {OracleMock} from "test/mock/OracleMock.sol";
 import {IrmMock} from "test/mock/IrmMock.sol";
 import {ActionsRouter} from "test/utils/ActionsRouter.sol";
+import {V4MiniRouter} from "test/utils/V4MiniRouter.sol";
+import {V4RouterHelper} from "test/utils/V4RouterHelper.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 
 contract Deployers is Test {
@@ -17,6 +19,8 @@ contract Deployers is Test {
     TestERC20 public tokenMock;
     IrmMock public irm;
     OracleMock public oracle;
+    V4MiniRouter public v4MiniRouter;
+    V4RouterHelper public v4RouterHelper;
 
     function deployAmpliWithActionRouter() public {
         address mockAmpli = address(0xFb46d30c9B3ACc61d714D167179748FD01E09aC0);
@@ -27,15 +31,21 @@ contract Deployers is Test {
     }
 
     function deployFreshManager() public {
+        vm.label(address(0x498581fF718922c3f8e6A244956aF099B2652b2b), "V4PoolManager");
         manager = IPoolManager(address(0x498581fF718922c3f8e6A244956aF099B2652b2b));
     }
 
     function deployMockERC20() public {
-        tokenMock = new TestERC20("Test Token", "TST", 18);
+        tokenMock = new TestERC20{salt: hex"0ff0"}("Test Token", "TST", 18);
     }
 
     function deployIrmAndOracle() public {
         irm = new IrmMock();
         oracle = new OracleMock();
+    }
+
+    function deployV4MiniRouter() public {
+        v4MiniRouter = new V4MiniRouter(address(manager));
+        v4RouterHelper = new V4RouterHelper(v4MiniRouter);
     }
 }
